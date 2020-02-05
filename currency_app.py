2fdf3884c2581d8b9c2e89e99baae2d173770fda
Mazzart <am.magicsmile@gmail.com>
2020-02-05 15:29:45 +0200

    Add functionality to get dict with currency buy/sale values

--- currency_app.py
+++ currency_app.py
@@ -1,11 +1,37 @@
+import logging
 from gazpacho import get, Soup
 
 ENDPOINT = 'https://obmenka24.kiev.ua/ua'
+DEFAULT_CURRENCY = 'USD/UAH'
 
-def main(ENDPOINT: str):
-    response = get(ENDPOINT)
-    soup = Soup(response)
+# endpoint classes names
+NAME = 'currencies__block-name'
+BUY = 'currencies__block-buy'
+SALE = 'currencies__block-sale'
+NUM = 'currencies__block-num'
+
+
+def get_currency_rates(url: str) -> dict:
+    result = {}
+
+    try:
+        response = get(url)
+        soup = Soup(response)
+
+        currencies = soup.find('span', {'class': NAME})
+        buy = soup.find('span', {'class': BUY})
+        buy_values = [value.find('div', {'class': NUM}).text for value in buy]
+        sale = soup.find('span', {'class': SALE})
+        sale_values = [value.find('div', {'class': NUM}).text for value in sale]
+
+        for cur, buy_num, sale_num in zip(currencies, buy_values, sale_values):
+            result[cur.text] = {'buy': float(buy_num), 'sale': float(sale_num)}
+
+    except Exception as err:
+        logging.exception(err)
+
+    return result
 
 
 if __name__ == '__main__':
-    main()
+    print(get_currency_rates(ENDPOINT))
