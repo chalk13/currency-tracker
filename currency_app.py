6eec85e8d7a89444a52696b0bab197b65a2c2de9
Mazzart <am.magicsmile@gmail.com>
2020-02-07 15:01:52 +0200

    Add arguments to list all currencies and change default one. Code refactoring

--- currency_app.py
+++ currency_app.py
@@ -1,12 +1,33 @@
+import argparse
 import logging
-import schedule
 import time
+
+import schedule
 from gazpacho import get, Soup
 from klaxon import klaxonify
+
 from config import ENDPOINT, DEFAULT_CURRENCY, TODAY, NAME, BUY, SALE, NUM
 
 
-def get_currency_rates(url: str) -> dict:
+def argument_parser():
+    """Initialize argument parser"""
+
+    description = "Currency tracker is tool which allows user to " \
+                  "get currency rates on regular basis"
+
+    parser = argparse.ArgumentParser(description=description)
+    parser.add_argument('--all', type=bool, default=False, nargs='?',
+                        choices=[True, False],
+                        help='get list of availiable currencies')
+    parser.add_argument('--change', type=str, nargs='?',
+                        help='change default currency to desired')
+
+    return parser
+
+
+def get_currencies_rates(url: str) -> dict:
+    """Return dictionary with exchange rates"""
+
     result = {}
 
     try:
@@ -45,18 +66,32 @@ def get_specific_currency_info(rates: dict, curr: str = DEFAULT_CURRENCY):
         return 'Check currency name and try again.'
 
 
-def every_day_notification():
+def every_day_notification(curr: str = DEFAULT_CURRENCY):
     """Send notification with actual rates"""
 
-    result = get_currency_rates(ENDPOINT)
-    get_specific_currency_info(result)
+    result = get_currencies_rates(ENDPOINT)
+    get_specific_currency_info(result, curr)
 
 
-# scheduler to send notifications every hour
-schedule.every().hour.do(every_day_notification)
+def main():
+    """Script entry point"""
 
+    args = argument_parser().parse_args()
+    argv = vars(args)
+    if argv['all']:
+        result = get_currencies_rates(ENDPOINT)
+        print(get_all_currencies(result))
 
-if __name__ == '__main__':
+    # scheduler to send notifications every hour
+    if argv['change']:
+        schedule.every().hour.do(every_day_notification, curr=argv['change'])
+    else:
+        schedule.every().hour.do(every_day_notification)
+
     while True:
         schedule.run_pending()
         time.sleep(1)
+
+
+if __name__ == '__main__':
+    main()
